b2df505369fc9c1c842247f7e9d2218e4b4ff854
LanceLionheart <lzvogel@outlook.com>
2020-08-31 13:34:57 -0500

    Added Data Layer, edmx, and metadata folder

---
+++ LMSFinalProject.DATA.EF/Metadata/LMSFinalMetadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMSFinalProject.DATA.EF.Metadata
+{
+    #region Course Metadata
+    public class CourseMetadata
+    {
+        //public int CourseId { get; set; }
+
+        [Display(Name = "Course Name")]
+        [StringLength(200, ErrorMessage = "* Material must be 200 characters or less.")]
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+    #endregion
+
+}
